c54d6a5bc2b491866d79688130a691a9e5f3b2d3
bholusaini <bholusaini2112@gmail.com>
2025-01-10 12:43:21 +0530

    dbupdate

--- Data/db.js
+++ Data/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, 
+  })
 
 const db =  mongoose.connection;
 
